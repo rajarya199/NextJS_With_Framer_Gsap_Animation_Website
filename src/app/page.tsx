2937ad93938a0f29cab1b8dc74d26e86327c1fa9
rajarya199 <aryalrajan078@gmail.com>
2026-01-19 13:42:48 +0545

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Hero from "@/components/Hero";
-
+import RecentWorkSection from "@/components/RecentWorkSection";
+import WorkDisplay from "@/components/WorkDisplay";
 
 export default function Home() {
   return (
   <main>
 <Hero/>
+<RecentWorkSection/>
+<WorkDisplay/>
+
   </main>
   );
 }
