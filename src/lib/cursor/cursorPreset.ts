87347536499e757049ba4738695e55f5a256631d
rajarya199 <aryalrajan078@gmail.com>
2026-01-22 18:08:26 +0545

    cursor-p

---
+++ src/lib/cursor/cursorPreset.ts
@@ -0,0 +1,16 @@
+export const smokey = {
+  id: "smokey-cursor",
+  transparent: true,
+  curl: 45,
+  splatForce: 9000,
+  densityDissipation: 0.98,
+};
+
+export const glow = {
+  id: "smokey-cursor",
+  transparent: true,
+  curl: 0,
+  splatForce: 0,
+  densityDissipation: 1,
+  pressure: 0,
+};
